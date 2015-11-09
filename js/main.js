f1ad91dc6156f5bbb8c8cd2c0e0a3ea8bb9ca4ce
pnganga <pnganga05@gmail.com>
2015-11-09 14:34:41 -0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
-main.js
+$(document).ready(function()
+	{
+		$("#st").hide();
+		$("#nd").hide();
+		$("#rd").hide();
+
+
+		$("#btn1").mouseenter(function(){
+
+			$("#st").show();
+
+
+		});
+
+		$("#btn2").mouseenter(function(){
+			$("#nd").show();
+
+
+		});
+
+		$("#btn3").mouseenter(function(){
+			$("#rd").show();
+
+
+		});
+
+		$("#btn3").mouseleave(function(){
+			$("#rd").hide();
+
+
+		});
+
+		$("#btn1").mouseleave(function(){
+			$("#st").hide();
+
+
+		});
+
+		$("#btn2").mouseleave(function(){
+			$("#nd").hide();
+
+
+		});
+
+
+
+
+	});	
